f403f8ca260c4f0899894d30459468c8f6fc39fa
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-11-19 10:33:37 +0000

    Add test scripts to check whether WGSLsmith works with the latest Dawn

---
+++ tests/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WGSLSMITH='../target/release/wgslsmith'
+
+$WGSLSMITH gen --recondition -o test.wgsl
+
+LINE="$(head -n 1 test.wgsl)"
+INPUTS="${LINE:3}"
+echo $INPUTS > inputs.json
+
+$WGSLSMITH run test.wgsl inputs.json --config 'dawn:vk:7425'
+
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+    echo "Success!"
+else 
+    echo "Failure! Return code: $RESULT"
+fi
